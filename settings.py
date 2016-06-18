2d26c86d8187dba168483516b25f7d980b608328
Ryan Zander <exide@hotmail.com>
2016-06-18 13:26:06 -0700

    Fixed some bugs introduced when moving to Django 1.4.

--- settings.py
+++ settings.py
@@ -36,9 +36,10 @@ USE_I18N = True
 USE_L10N = True
 MEDIA_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'media')
 MEDIA_URL = '/media/'
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
 SECRET_KEY = 'you-should-generate-a-good-hash-for-this'
-AUTHENTICATION_BACKENDS = 'account.backends.CaseInsensitiveBackend'
+AUTHENTICATION_BACKENDS = ('account.backends.CaseInsensitiveBackend',)
 PAGINATION_DEFAULT_WINDOW = 5
 
 ROOT_URLCONF = 'urls'
@@ -66,6 +67,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.staticfiles',
     'django.contrib.messages',
     'django.contrib.admin',
     'django.contrib.admindocs',
